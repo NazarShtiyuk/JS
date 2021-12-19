51a66d6cc1265801e098435ca726d3dd05defe14
NazarShtiyuk <nazarshtiyuk@gmail.com>
2021-12-19 13:25:05 +0200

    lesson13(correct mistake)

--- lesson13/additional13.js
+++ lesson13/additional13.js
@@ -3,14 +3,18 @@ let formItem = document.forms['formItem'];
 let itemsArr = [];
 localStorage.setItem('items', JSON.stringify(itemsArr));
 formItem.btn.onclick = function (e) {
+    e.preventDefault();
     let item = {
         nameItem: formItem.name.value,
         countItem: formItem.count.value,
         priceItem: formItem.price.value,
         photoItem: formItem.photo.value
     }
-    e.preventDefault();
     itemsArr = JSON.parse(localStorage.getItem('items'));
     itemsArr.push(item);
     localStorage.setItem('items', JSON.stringify(itemsArr));
+    formItem.name.value = '';
+    formItem.count.value = '';
+    formItem.price.value = '';
+    formItem.photo.value = '';
 }
