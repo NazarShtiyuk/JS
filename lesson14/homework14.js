ccf97c76d1c11990014e502085b86c76a4089854
NazarShtiyuk <nazarshtiyuk@gmail.com>
2021-12-22 17:56:41 +0200

    lesson14(added solution via PromiseThen and Callback)

--- lesson14/homework14.js
+++ lesson14/homework14.js
@@ -1,4 +1,5 @@
-function getUp(ifGetUp) {
+let ifGetUp = true;
+function getUp(ifGetUp, callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (ifGetUp) {
@@ -6,46 +7,51 @@ function getUp(ifGetUp) {
                 resolve(true);
             }
             reject(false);
+            // callback();
         }, 1000)
     })
 }
 
-function getBrushTeeth(ifGetUp) {
+function getBrushTeeth(ifGetUp, callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (ifGetUp) {
+                // callback()
                 resolve('Почистив зуби');
             }
         }, 1000)
     })
 }
 
-function getWashIn() {
+function getWashIn(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // callback()
             resolve('Вмився');
         }, 500)
     })
 }
 
-function getBreakfast() {
+function getBreakfast(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // callback()
             resolve('Поснідав');
         }, 2000)
     })
 }
 
-function goWork() {
+let money = 150;
+function goWork(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            let money = 150;
             console.log('Приїхав на роботу');
+            // callback();
             resolve(money);
         }, 3000)
     })
 }
-function getLunch(money) {
+function getLunch(money, callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (money > 100) {
@@ -53,26 +59,30 @@ function getLunch(money) {
             } else {
                 resolve('Забувсь готівку вдома :(, тому залишивсь голодний')
             }
+            // callback()
         }, 2000)
     })
 }
-function goHome() {
+function goHome(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // callback();
             resolve('Повернувсь додому');
         }, 3000)
     })
 }
-function doHomework() {
+function doHomework(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // callback()
             resolve('Зробив домашню OKTEN`a');
         }, 3000)
     })
 }
-function goToBed() {
+function goToBed(callback) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // callback()
             resolve('Підготовка до сну');
         }, 1000)
     })
@@ -105,38 +115,69 @@ function goToBed() {
 
 
 //                      promise then
-// getUp(true)
-//     .then(task1 => {
-//         return getBrushTeeth(task1);
-//     })
-//     .then(task2 => {
-//         console.log(task2);
-//         return getWashIn()
-//     })
-//     .then(task3 => {
-//         console.log(task3);
-//         return getBreakfast();
-//     })
-//     .then(task4 => {
-//         console.log(task4);
-//         return goWork();
-//     })
-//     .then(task5 => {
-//         return getLunch(task5);
-//     })
-//     .then(task6 => {
-//         console.log(task6);
-//         return goHome();
-//     })
-//     .then(task7 => {
-//         console.log(task7);
-//         return doHomework();
-//     })
-//     .then(task8 => {
-//         console.log(task8);
-//         return goToBed();
-//     })
-//     .then(task9 => {
-//         console.log(task9);
-//     })
+getUp(true)
+    .then(task1 => {
+        return getBrushTeeth(task1);
+    })
+    .then(task2 => {
+        console.log(task2);
+        return getWashIn()
+    })
+    .then(task3 => {
+        console.log(task3);
+        return getBreakfast();
+    })
+    .then(task4 => {
+        console.log(task4);
+        return goWork();
+    })
+    .then(task5 => {
+        return getLunch(task5);
+    })
+    .then(task6 => {
+        console.log(task6);
+        return goHome();
+    })
+    .then(task7 => {
+        console.log(task7);
+        return doHomework();
+    })
+    .then(task8 => {
+        console.log(task8);
+        return goToBed();
+    })
+    .then(task9 => {
+        console.log(task9);
+    })
 
+//                          callback
+// getUp(ifGetUp, () => {
+//     getBrushTeeth(ifGetUp, () => {
+//         console.log('Почистив зуби')
+//         getWashIn(() => {
+//             console.log('Вмився')
+//             getBreakfast(() => {
+//                 console.log('Поснідав')
+//                 goWork(() => {
+//                     // console.log('Приїхав на роботу')
+//                     getLunch(money, () => {
+//                         if (money > 100) {
+//                             console.log('Пообідав');
+//                         } else {
+//                             console.log('Забувсь готівку вдома :(, тому залишивсь голодний')
+//                         }
+//                         goHome(() => {
+//                             console.log('Повернувсь додому');
+//                             doHomework(() => {
+//                                 console.log('Зробив домашню OKTEN`a');
+//                                 goToBed(() => {
+//                                     console.log('Підготовка до сну')
+//                                 })
+//                             })
+//                         })
+//                     })
+//                 })
+//             })
+//         });
+//     });
+// });
